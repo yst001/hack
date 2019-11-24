c1e550325696b84a34501cfc39b8d6339d66ec55
yst001 <44132305+yst001@users.noreply.github.com>
2019-11-24 10:14:17 +0800

    1. 增加悬浮导航功能，About和Connect已经添加，index将我的index里body的代码全部复制到body中 2. images添加图片right_bg.png 3. 添加Connect的html和css
    Signed-off-by: yst001 <44132305+yst001@users.noreply.github.com>

--- hack/js/index.js
+++ hack/js/index.js
@@ -1,1 +1,5 @@
-var x=5;
+$(function () {
+    SuspendedBall.Add();//添加悬浮球
+    SuspendedBall.BoxHtml("<ul class=\"SMenu\">" + $("#MenuInfo").html() + "</ul>");//添加菜单框的内容当然，具体的样式和里面的内容需要自己去写
+    SuspendedBall.Move();//这个就是让悬浮球动起来的方法。为啥单独写个不写到add方法里面呢？因为你可以在页面中直接写入悬浮球的两个div。这个方法里面可以添加一个参数，这个参数是个function。当鼠标抬起的时候会调用到这个方法。
+});
